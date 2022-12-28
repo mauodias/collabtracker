86e34326acc0ba72004aa2a4870bf48573a05cd9
Maurício Dias <mau.dias@gmail.com>
2022-12-28 21:05:53 +0100

    feat: add empty endpoints

--- service/app/app.py
+++ service/app/app.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/users")
+async def get_users():
+    return {"users": []}
+
+@app.put("/users")
+async def add_user(user: str = Form()):
+    return {"users": []}
+
+@app.delete("/users")
+async def delete_user(user: str):
+    return {"users": []}
+
+@app.get("/me")
+async def get_logged_user():
+    return {"user": ""}
